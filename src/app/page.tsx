ac8b1a04dc6fd32d541af8e0026fff83a93273e6
Dmitry Kuchukov <kudim@inbox.ru>
2024-10-09 17:01:09 +0300

    feat: add shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,15 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+
 function HomePage() {
-  return <h1 className="text-3xl">HomePage</h1>;
+  return (
+    <>
+      <h1 className="text-3xl">HomePage</h1>
+      <Input type="email" placeholder="Email" />
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        Click Me
+      </Button>
+    </>
+  );
 }
 export default HomePage;
---
+++ src/app/reviews/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ReviewsPAge() {
+  return <div>ReviewsPAge</div>;
+}
