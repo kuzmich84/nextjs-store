11f741d79c73e4dcbfb011e4e72e47ee01f44d1f
Dmitry Kuchukov <kudim@inbox.ru>
2024-10-13 20:48:01 +0300

    feat: add products grid component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import db from '@/utils/db';
 
-export default function AboutPage() {
-  return <div>AboutPage</div>;
+export default async function AboutPage() {
+  await db.testProfile.create({
+    data: {
+      name: 'First name',
+    },
+  });
+  const users = await db.testProfile.findMany();
+
+  return (
+    <div>
+      {users.map((user) => {
+        return (
+          <h2 key={user.id} className="text-2xl font-bold">
+            {user.name}
+          </h2>
+        );
+      })}
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import FeaturedProducts from '@/components/home/FeaturedProducts';
+import Hero from '@/components/home/Hero';
 
 function HomePage() {
   return (
     <>
-      <h1 className="text-3xl">HomePage</h1>
-      <Input type="email" placeholder="Email" />
-      <Button variant="outline" size="lg" className="capitalize m-8">
-        Click Me
-      </Button>
+      <Hero />
+      <FeaturedProducts />
     </>
   );
 }
